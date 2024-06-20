6e5b166bddf1c5daa265a58ca37aa83332bf7520
A1exRS <alubascik@gmail.com>
2024-06-21 00:53:11 +0200

    New Player redesign

--- app/game/components/NewPlayer.jsx
+++ app/game/components/NewPlayer.jsx
@@ -1,16 +1,24 @@
 import styles from "@styles/NewPlayer.module.sass";
-import React from "react";
+import React, { useState } from "react";
 import { setPlayerCookie } from "./actions/setPlayerCookie";
 
 const NewPlayer = ({ socket, gameData, setCurrentUUID }) => {
+  const [error, setError] = useState(false)
   function handleSubmit(e) {
     e.preventDefault();
     const formData = new FormData(e.target);
-    socket.emit("create-newPlayer", { playerName: formData.get("playerName") }, (data) => {
+    const name = formData.get("playerName")
+    console.log(`"${name}"`);
+    if (!name || name === "") {
+      setError("The name is required to join the session!");
+      return;
+    }
+    socket.emit("create-newPlayer", { playerName: name }, (data) => {
       if (data.error === undefined) {
         setCurrentUUID(data.uuid);
         setPlayerCookie(data);
       } else {
+        setError(data.error)
         console.log(data.error);
       }
     });
@@ -19,20 +27,13 @@ const NewPlayer = ({ socket, gameData, setCurrentUUID }) => {
   return (
     <div className={styles["newPlayer-main"]}>
       <div className={styles["newPlayer-container"]}>
-        <h3>Welcome! Please give us your name so others can identify you during the game.</h3>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor='playerName'>Name:</label>
-          <input className={styles.error} type='text' id='playerName' name='playerName' />
+        <h3>Please give us your name so others know how to call you during the session.</h3>
+        <form onSubmit={handleSubmit} autoComplete="off" >
+          {error ?
+            <p className={styles.errorMessage}>{error}</p> : <br />
+          }
+          <input className={error ? styles.error : ""} placeholder="Name" type='text' id='playerName' name='playerName' />
           <button type='submit'>Start Playing!</button>
-          {/* <label htmlFor="color">Color:</label>
-                    <select name="color" id="color">
-                        <option value="green">Green</option>
-                        <option value="red">Red</option>
-                        <option value="blue">Blue</option>
-                        <option value="yellow">Yellow</option>
-                        <option value="purple">Purple</option>
-                        <option value="pink">Pink</option>
-                    </select> */}
         </form>
       </div>
     </div>
